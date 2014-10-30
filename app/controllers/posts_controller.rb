acfdc585dfb8e718bd6fa746463199a8228c7374
MitsunChieh <Mitsun.Chieh@gmail.com>
2014-10-30 16:17:16 +0800

    Add login and create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,29 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user = current_user
+
+    if @topic.save
+      redirect_to posts_url
+    else
+      render :action => :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :content, :category_id, :user_id)
   end
 
 end
