3b8ec4d65f53cea848f45a027cad2dea306cc0d7
MitsunChieh <Mitsun.Chieh@gmail.com>
2014-10-30 19:00:34 +0800

    Add name in advise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name  # devise增加name欄位
+    devise_parameter_sanitizer.for(:account_update) << :name  # 可以修改user的資料
+  end
 end
